4a2b57ec003af3323f600e39ea9827366f759838
pauldhz <paul.denhez.ds@gmail.com>
2025-12-13 16:18:53 +0100

    add simple verify service

--- src/app/auth/service/verify-service.ts
+++ src/app/auth/service/verify-service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VerifyService {
-  
+
+  private http = inject(HttpClient);
+  private readonly API_URL = 'api/auth';
+
+  verifyToken(): Observable<any> {
+    return this.http.get(`${this.API_URL}/verify`);
+  }
 }
